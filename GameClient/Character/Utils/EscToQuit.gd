d9e0cde211414c7d68f4412c277f8c7f1a87b0b8
Tom Lechner <tom@tomlechner.com>
2022-06-07 19:22:19 -0700

    Clean up deleted files.

--- GameClient/Character/Utils/EscToQuit.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-# Catch ESC key on key up. use get_viewport().set_input_as_handled() to prevent an ESC press from quiting.
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.physical_keycode == KEY_ESCAPE && event.pressed == false:
-			get_tree().quit()
