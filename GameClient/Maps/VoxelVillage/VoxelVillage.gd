be8c5b876b508c3fd0559396c948f292668a789e
unblinky <47279470+unblinky@users.noreply.github.com>
2022-07-30 23:29:03 -0700

    Voxeling Add and Drill voxels in the landscape.

--- GameClient/Maps/VoxelVillage/VoxelVillage.gd
+++ GameClient/Maps/VoxelVillage/VoxelVillage.gd
@@ -1,8 +1,21 @@
 extends Node3D
 
 
+# Probably where to handle voxel addition.
+func AddVoxel(voxel):
+	$Voxeling.position.y += 0.1 # Move the player before spawning a voxel.
+	voxel.position.y += 0.05 # Raise voxel.
+	
+	# Snap to grid or whatever.
+	voxel.position.x = ceil((voxel.position.x - 0.05) * 10) / 10
+	voxel.position.z = ceil((voxel.position.z - 0.05) * 10) / 10
+	
+	$Landscape.add_child(voxel)
+
+
 func SaveLandscape():
 	pass
+
 
 func ResetLandscape():
 	pass
